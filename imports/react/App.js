0bfec44440e1badca05d1a3ab40c13aba0128f20
Anton Karpenko <karpolan@karpolan.com>
2019-08-29 18:27:40 +0200

    Incomplete Tasks filter and count added

--- imports/react/App.js
+++ imports/react/App.js
@@ -9,6 +9,20 @@ import Task from './Task.js';
  * Represents the whole app
  */
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+
+  toggleHideCompleted = (event) => {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  };
+
   handleSubmit = (event) => {
     event.preventDefault();
 
@@ -44,21 +58,41 @@ class App extends Component {
     return [{ _id: 1, text: 'This is task 1' }, { _id: 2, text: 'This is task 2' }, { _id: 3, text: 'This is task 3' }];
   }
 
-  renderTasks() {
-    return this.props.tasks.map((task) => <Task key={task._id} task={task} />);
+  /**
+   * Filters and renders list of given task.
+   * @param {array} tasks - list of task to filter and render.
+   * @param {bool} hideCompleted - flag to not show the complete tasks.
+   */
+  renderTasks(tasks, hideCompleted) {
+    let filteredTasks = [];
+    if (hideCompleted) {
+      filteredTasks = tasks.filter((task) => !task.checked);
+    } else {
+      filteredTasks = tasks;
+    }
+    return filteredTasks.map((task) => <Task key={task._id} task={task} />);
   }
 
   render() {
+    const { tasks, incompleteCount } = this.props;
+    const { hideCompleted } = this.state;
+
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({incompleteCount})</h1>
+
+          <label className="hide-completed">
+            <input type="checkbox" readOnly checked={hideCompleted} onClick={this.toggleHideCompleted} />
+            Hide Completed Tasks
+          </label>
+
           <form className="new-task" onSubmit={this.handleSubmit}>
             <input type="text" ref="textInput" placeholder="Type to add new tasks" />
           </form>
         </header>
 
-        <ul>{this.renderTasks()}</ul>
+        <ul>{this.renderTasks(tasks, hideCompleted)}</ul>
       </div>
     );
   }
@@ -67,6 +101,7 @@ class App extends Component {
 const AppWithTracker = withTracker(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
   };
 })(App);
 
