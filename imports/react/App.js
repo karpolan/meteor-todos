dc56998efdbc38b91395405b673321e24c3b232c
Anton Karpenko <karpolan@karpolan.com>
2019-08-29 17:52:54 +0200

    Task adding form added

--- imports/react/App.js
+++ imports/react/App.js
@@ -1,11 +1,43 @@
-import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 import { Tasks } from '../api/tasks.js';
 import Task from './Task.js';
 
 // App component - represents the whole app
 class App extends Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Get input value
+    const inputText = event.target[0];
+    const text = inputText.value.trim();
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear old value
+    inputText.value = '';
+  };
+
+  handleSubmitUsingRefs = (event) => {
+    event.preventDefault();
+
+    // Find the text field via the React ref
+    const textInput = ReactDOM.findDOMNode(this.refs.textInput);
+    const text = textInput.value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    textInput.value = '';
+  };
+
   getTasks() {
     return [{ _id: 1, text: 'This is task 1' }, { _id: 2, text: 'This is task 2' }, { _id: 3, text: 'This is task 3' }];
   }
@@ -19,6 +51,9 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <form className="new-task" onSubmit={this.handleSubmit}>
+            <input type="text" ref="textInput" placeholder="Type to add new tasks" />
+          </form>
         </header>
 
         <ul>{this.renderTasks()}</ul>
@@ -29,7 +64,7 @@ class App extends Component {
 
 const AppWithTracker = withTracker(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 })(App);
 
