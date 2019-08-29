40ea6b7f636f411ba2305bbd8ec8a23065617e38
Anton Karpenko <karpolan@karpolan.com>
2019-08-29 20:46:52 +0200

    fix

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -34,7 +34,7 @@ Meteor.methods({
     check(taskId, String);
 
     const task = Tasks.findOne(taskId);
-    if (task.private && task.owner == this.userId) {
+    if (task.private && task.owner !== this.userId) {
       // If the task is private, make sure only the owner can delete it
       throw new Meteor.Error('not-authorized');
     }
